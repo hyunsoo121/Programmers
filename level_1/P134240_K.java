dcabbf3afc56b2a7f47395ac51faffeb5431fe01
ggom121@naver.com <ggom121@naver.com>
2025-04-08 22:12:54 +0900

    P134240_K

---
+++ level_1/P134240_K.java
@@ -0,0 +1,21 @@
+package Programmers.level_1;
+
+public class P134240_K {
+    class Solution {
+        public String solution(int[] food) {
+            String answer = "";
+            String reverseAnswer = "";
+
+            for(int i = 1; i < food.length; i++){
+                for(int j = 0; j < food[i] / 2; j++){
+                    answer += i;
+                    reverseAnswer = i + reverseAnswer;
+                }
+            }
+
+            answer = answer + 0 + reverseAnswer;
+
+            return answer;
+        }
+    }
+}
