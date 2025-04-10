f9f7446d5eac95e644db6423f40cbb1ca2c0ff4c
ggom121@naver.com <ggom121@naver.com>
2025-04-10 13:27:28 +0900

    P138477_K

---
+++ level_1/P138477_K.java
@@ -0,0 +1,26 @@
+package Programmers.level_1;
+
+import java.util.*;
+public class P138477_K {
+    class Solution {
+        public int[] solution(int k, int[] score) {
+            int[] answer = new int[score.length];
+
+            ArrayList<Integer> arr = new ArrayList<>();
+
+            for (int i = 0; i < score.length; i++){
+
+                arr.add(score[i]);
+                Collections.sort(arr);
+
+                if (arr.size() > k){
+                    arr.remove(0);
+                }
+
+                answer[i] = arr.get(0);
+            }
+
+            return answer;
+        }
+    }
+}
