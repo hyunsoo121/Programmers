49a96cc4312b4d57c2da773117897675aaadbed6
wonbin <wonbin3977@gmail.com>
2025-04-10 22:10:02 +0900

    P81301_W

--- level_1/P81301_W.java
+++ level_1/P81301_W.java
@@ -9,7 +9,7 @@ class Solution {
         HashMap<String,Integer> hm=new HashMap<>();
         ArrayList<Integer> al=new ArrayList<>();
         hm.put("zero",0);
-        hm.put("one",1); hm.put("two",2); hm.put("three",3);
+        hm.put("one",1); hm.put("two",2); hm.put("three",3); //
         hm.put("four",4); hm.put("five",5); hm.put("six",6);
         hm.put("seven",7); hm.put("eight",8); hm.put("nine",9);
         char[] c=s.toCharArray();
