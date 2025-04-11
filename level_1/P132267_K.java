91b4217e7de8a753fd95a3f428f33c3eca2addcd
ggom121@naver.com <ggom121@naver.com>
2025-04-11 17:21:55 +0900

    P132267_K

---
+++ level_1/P132267_K.java
@@ -0,0 +1,19 @@
+package Programmers.level_1;
+
+import java.util.*;
+
+public class P132267_K {
+    class Solution {
+        public int solution(int a, int b, int n) {
+            int answer = 0;
+            int d = n;
+            while (d >= a){
+                int takeBottle = (d / a) * b;;
+                d %= a;
+                answer += takeBottle;
+                d += takeBottle;
+            }
+            return answer;
+        }
+    }
+}
