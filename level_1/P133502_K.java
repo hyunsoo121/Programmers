638f6b741ceb80ae3fe54d2b2f5832cb26fb0b49
ggom121@naver.com <ggom121@naver.com>
2025-04-29 23:30:41 +0900

    P133502_K

---
+++ level_1/P133502_K.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class P133502_K {
+    class Solution {
+        public int solution(int[] ingredient) {
+            int answer = 0;
+            Stack<Integer> stack = new Stack<>();
+
+            for (int i : ingredient) {
+                stack.push(i);
+
+                if (stack.size() >= 4) {
+                    int size = stack.size();
+                    if (stack.get(size - 4) == 1 &&
+                            stack.get(size - 3) == 2 &&
+                            stack.get(size - 2) == 3 &&
+                            stack.get(size - 1) == 1) {
+
+                        for (int j = 0; j < 4; j++) {
+                            stack.pop();
+                        }
+
+                        answer++;
+                    }
+                }
+            }
+
+            return answer;
+        }
+    }
+
+}
