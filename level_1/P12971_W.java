bba26ef1e8f0c5f6cd704de81513fb1234d83473
wonbin <wonbin3977@gmail.com>
2025-05-25 00:25:19 +0900

    P12971_W

---
+++ level_1/P12971_W.java
@@ -0,0 +1,27 @@
+public class P12971_W 
+{
+    class Solution {
+    public int solution(int sticker[]) 
+    {
+        int n=sticker.length;
+        if(n==1) return sticker[0];
+        if(n==2) return Math.max(sticker[0],sticker[1]);
+        int[] dp1=new int[n]; //첫번째 스티커 뜯음
+        int[] dp2=new int[n]; //첫번째 안뜯음
+        
+        dp1[0]=sticker[0]; //-> 마지막꺼 못뜯음
+        dp1[1]=dp1[0];
+        for(int i=2;i<n-1;i++)
+        {
+            dp1[i]=Math.max(dp1[i-1],dp1[i-2]+sticker[i]);
+        }
+        dp2[0]=0;
+        dp2[1]=sticker[1];
+        for(int i=2;i<n;i++)
+        {
+            dp2[i]=Math.max(dp2[i-1],dp2[i-2]+sticker[i]);
+        }
+        return Math.max(dp1[n-2],dp2[n-1]);
+    }
+}
+}
